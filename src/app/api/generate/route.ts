35d63e2bec70c8675df4ff1abe08f2c89274e9ae
adityan <adityannair007@gmail.com>
2026-02-06 15:16:55 +0530

    docker files and redis files setup, redis setup for saving food item details as cache with recipes

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,11 +1,13 @@
-import { redis } from "@/lib/redis";
 import { GoogleGenAI } from "@google/genai";
 import { NextRequest, NextResponse } from "next/server";
+import { redis } from "@/lib/redis";
+import { createClient } from "redis";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function POST(req: NextRequest) {
   try {
+    console.log("VERSION 2.0 - DOCKER UPDATED");
     const { ingredients, allergies, cuisine, mealType, diet } =
       await req.json();
 
@@ -17,17 +19,17 @@ export async function POST(req: NextRequest) {
       diet,
     })}`;
 
-    const cachedRecipes = await redis.get(cacheKey);
-    if (cachedRecipes) {
-      console.log("Serving recipes from redis cache!");
-      return NextResponse.json({ items: cachedRecipes });
+    const cacheRecipes = await redis.get(cacheKey);
+
+    if (cacheRecipes) {
+      console.log("Serving from redis cache!");
+      return NextResponse.json({ items: JSON.parse(cacheRecipes) });
     }
 
     const res = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: `Generate 3 recipes as a JSON array. 
+      contents: `Generate ${Array.isArray(mealType) ? mealType.join(", ") : mealType} recipes as a JSON array. 
         Diet: ${diet || "Any"}
-        Meal Types: ${Array.isArray(mealType) ? mealType.join(", ") : mealType}
         Cuisines: ${Array.isArray(cuisine) ? cuisine.join(", ") : cuisine}
         Ingredients: ${ingredients.join(", ")}
         Exclude: ${allergies?.join(", ") || "None"}
@@ -39,22 +41,22 @@ export async function POST(req: NextRequest) {
 
     const items = JSON.parse(res.text || "[]");
 
-    await redis.set(cacheKey, items, { ex: 86400 });
+    await redis.set(cacheKey, JSON.stringify(items), { EX: 86400 });
 
     return NextResponse.json({ items });
   } catch (error: any) {
-    console.error("SDK Error:", error);
+    console.error("Error:", error);
 
     if (error.status === 429 || error.message?.includes("429")) {
       return NextResponse.json(
         { error: "Rate limit reached" },
-        { status: 429 }
+        { status: 429 },
       );
     }
 
     return NextResponse.json(
       { error: "Failed to generate recipes", details: error.message },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,18 @@
-import { Redis } from "@upstash/redis";
+import { createClient } from "redis";
 
-export const redis = Redis.fromEnv();
+// This prevents multiple connections during Next.js Hot Reloads
+const globalForRedis = global as unknown as {
+  redis: ReturnType<typeof createClient>;
+};
+
+export const redis =
+  globalForRedis.redis ||
+  createClient({
+    url: process.env.REDIS_URL,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForRedis.redis = redis;
+
+if (!redis.isOpen) {
+  redis.connect().catch(console.error);
+}
