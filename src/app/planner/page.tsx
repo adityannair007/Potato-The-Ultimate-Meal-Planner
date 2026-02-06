d416596b2483c9d8db93f93ae052835787671361
adityan <adityannair007@gmail.com>
2026-02-06 23:05:51 +0530

    planning meals based on mealtype, diet preference, cuisine, ingredients and allergies

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -1,9 +1,30 @@
+import { Item } from "../fridge/page";
+import { createClient } from "../lib/supabase/server";
+import { foodRecipe } from "../types/recipe";
 import PlannerClient from "./plannerClient";
 
 export default async function Planner() {
+  const supabase = await createClient();
+
+  const redis = createClient;
+
+  const [fridgeItems, allergens] = await Promise.all([
+    supabase.from("fridge").select("id, name"),
+    supabase.from("allergy").select("id, name"),
+  ]);
+
+  if (fridgeItems.error || allergens.error) {
+    console.error(
+      "Error fetching ingredients and allergens: ",
+      fridgeItems.error || allergens.error,
+    );
+  }
   return (
     <>
-      <PlannerClient />
+      <PlannerClient
+        initialFridgeItems={fridgeItems.data as Item[]}
+        initialAllergens={allergens.data as Item[]}
+      />
     </>
   );
 }
